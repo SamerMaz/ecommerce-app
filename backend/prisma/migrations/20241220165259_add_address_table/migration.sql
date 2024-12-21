8f3df507b950583ee6bfc856a614b1b4a638be1a
samer.mazraani <samer.mazraani96@gmail.com>
2024-12-21 17:33:10 +0200

    add model address

---
+++ backend/prisma/migrations/20241220165259_add_address_table/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `products` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateTable
+CREATE TABLE "addresses" (
+    "id" SERIAL NOT NULL,
+    "lineOne" TEXT NOT NULL,
+    "lineTwo" TEXT,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "postalCode" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "addresses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_name_key" ON "products"("name");
